2c10ed084a069824a0d643b5b8a59a38675ec94e
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-05-27 11:44:33 +0530

    feat: calling mock server for testing and displayed output

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtEnglish = document.querySelector("#txt-english");
 var txtBanana = document.querySelector("#txt-banana");
 
-function clickEventHandler(){
-    txtBanana.innerText = "English : " + txtEnglish.value;
+var serverURL = "https://lessonfourapi.gomathicoder.repl.co/translate/yoda.json";
+
+function getTranslateURL(text){
+    return serverURL + "?text=" + text;
 }
 
-btnTranslate.addEventListener("click", clickEventHandler);
+function clickHandler(){
+    var inputText = txtEnglish.value;
+
+    // Calling server for translation
+    fetch(getTranslateURL(inputText))
+    .then(response => response.json())
+    .then(json => txtBanana.innerText = json.contents.translated)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
